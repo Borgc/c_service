f11e5cfaba9cce9508585b23b908cdca6e94f7b0
Borgc <55150559+Borgc@users.noreply.github.com>
2019-12-13 15:59:26 +0700

    fix registration

--- service.c
+++ service.c
@@ -24,7 +24,7 @@ int user_exist(char *name)// check user
     return 0;
 }
 
-void sign_in()
+int sign_in()
 {
     char name[17];
     char password[17];
@@ -47,36 +47,42 @@ void sign_in()
     strcat(path, name);
     FILE *my_file;
     
+    if(user_exist(name) == 0)
+    {
+        printf("No such user\n");
+        return 0;
+    }
+   
+    int a = strlen(path) + strlen("/password");
+    char pass_path[a];
+    pass_path[0] = '\x00';
 
-    if(user_exist(name) == 1)
-    {   
-        int a = strlen(path) + strlen("/password");
-        char pass_path[a];
-        pass_path[0] = '\x00';
-        strcat(pass_path, path);
-        strcat(pass_path, "/password");
-        my_file = fopen(pass_path, "r");
-        char check_pass[17];
-        fgets(check_pass, 16, my_file);
-        int i = 0;
-        int equal = 1;
-        while(check_pass[i] != 0x0 || password[i] != 0x0){
-            if(check_pass[i] != password[i])equal = 0;
-            i++;
-        }
-        if(equal){
-            printf("Welcome home\n");
-            return; // сделать папку с планетками (x, y) матрицаб на каждой там бункеры с паролями, рандомная генерация ролла с пушками для убийства других плане
-                    // можно прыгать рандомно, можно по координатам, которые знаешь, занять плнету можно, если ты прилетел на нее первый и поставил свой пароль
-                    // взрывая чужую планету можнон получить секрет взорвавшегося(пароль от планеты) ++сделать планету pooper++
-                    // 
-        }
-        printf("Wrong password");
+    strcat(pass_path, path);
+    strcat(pass_path, "/password");
 
-    } 
+    my_file = fopen(pass_path, "r");
+    char check_pass[17];
+    fgets(check_pass, 16, my_file);
+    int i = 0;
+    int equal = 1;
+    while(check_pass[i] != 0x0 || password[i] != 0x0){
+        if(check_pass[i] != password[i])equal = 0;
+        i++;
+    }
+    if(equal){
+        printf("Welcome home\n");
+        return 1; // сделать папку с планетками (x, y) матрицаб на каждой там бункеры с паролями, рандомная генерация ролла с пушками для убийства других плане
+                // можно прыгать рандомно, можно по координатам, которые знаешь, занять плнету можно, если ты прилетел на нее первый и поставил свой пароль
+                // взрывая чужую планету можнон получить секрет взорвавшегося(пароль от планеты) ++сделать планету pooper++
+                // 
+    }
+    printf("Wrong password\n");
+    return 0;
+
 
 }
-void registration()
+
+int registration()
 {   
     char name[17];
     char password[17];
@@ -95,73 +101,83 @@ void registration()
     if(user_exist(name) == 1)
     {   
 
-        printf("Choose another login");
-        return;
+        printf("Choose another login\n");
+        return 0;
     } 
-    else {
-        int n = strlen(USERS_FOLDER) + strlen(name) + 1;
-        char path[n];
-        path[0] = '\x00';
+ 
+    n = strlen(USERS_FOLDER) + strlen(name) + 1;
+    char path[n];
+    path[0] = '\x00';
 
-        strcat(path, USERS_FOLDER);
-        strcat(path, name);
-        FILE *my_file;
-        mkdir(path, 0700);
-        int a = strlen(path) + strlen("/password");
-        char pass_path[a];
-        pass_path[0] = '\x00';
-        strcat(pass_path, path);
-        strcat(pass_path, "/password");
-        my_file = fopen(pass_path, "w");
-        if(my_file == NULL)
-        {
-            perror("sorry your file cant't be opened:");
-            return;
-        } 
+    strcat(path, USERS_FOLDER);
+    strcat(path, name);
+    FILE *my_file;
+    mkdir(path, 0700);
+    int a = strlen(path) + strlen("/password");
+    char pass_path[a];
+    pass_path[0] = '\x00';
+    strcat(pass_path, path);
+    strcat(pass_path, "/password");
+    my_file = fopen(pass_path, "w");
+    if(my_file == NULL)
+    {
+        perror("sorry your file cant't be opened:");
+        return 0 ;
+    } 
 
-        fprintf(my_file, "%s", password);
-        printf("Success");
-        
-        return;
-    }
+    fputs(password, my_file);
+    fclose(my_file);
+    printf("Success");
+    
+    return 1;
+    
 }
 
-void status()
-{
-
-}
+// void status()
+// {
+//     printf("You're inside\nNow you can continue your adventure(press h for help)\n");
+//     int choice2;
+//     switch(choice2){
+//         case 'h':
+//             printf("1 - random jump\n");
+//     }
+// }
 
 
 int main(void)
-{   
-
+{  
     int choice1;
     int trash;
-    //int status = 0;
+    int status_reg = 0;
     while(1)
     {
-        printf("sign in / regisration / exit? 1/2/0\n");
-        choice1 = getc(stdin);
-        while(trash != 0xa){
-            trash = getc(stdin);
-        }
-        while(choice1 != '1' && choice1 != '2'){
-            printf("Wrong input, try 1 or 2 maybe??\n");
+        if(status_reg == 0){
+            printf("sign in / regisration / exit? 1/2/0\n");
             choice1 = getc(stdin);
+            while(trash != 0xa){
+                trash = getc(stdin);
+            }
+            while(choice1 != '1' && choice1 != '2' && choice1 != '0'){
+                printf("Wrong input, try 1 or 2 maybe??\n");
+                choice1 = getc(stdin);
+            }
+            switch(choice1)
+            {
+                case '1': 
+                    status_reg = sign_in();
+                    break;
+                case '2': 
+                    status_reg = registration();
+                    break;
+                case '0':
+                    goto end;
+            }
         }
-        switch(choice1)
-        {
-            case '1': 
-                sign_in();
-                break;
-            case '2': 
-                registration();
-                break;
-            case '0':
-                goto end;
-        }
+
+        //if(status_reg == 1)status();
     }
     
-end:    
+end: 
+printf("bye\n");   
 return 0;
 }
